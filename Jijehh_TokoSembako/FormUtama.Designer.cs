1c618076d439bc1ffbcefff8c56e9737b5272568
ZidaneAlhakim <zidanealhakim179@gmail.com>
2026-04-13 22:41:42 +0700

    Add project files.

---
+++ Jijehh_TokoSembako/FormUtama.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Jijehh_TokoSembako
+{
+    partial class FormUtama
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvData
+            // 
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Location = new System.Drawing.Point(4, 515);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.RowHeadersWidth = 51;
+            this.dgvData.RowTemplate.Height = 24;
+            this.dgvData.Size = new System.Drawing.Size(1170, 256);
+            this.dgvData.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(108, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            
+            // 
+            // FormUtama
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1186, 783);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvData);
+            this.Name = "FormUtama";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FormUtama_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvData;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Jijehh_TokoSembako/FormUtama.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient; // Wajib untuk SQL Server
+using System.Windows.Forms;
+
+namespace Jijehh_TokoSembako // Nama namespace sesuai nama project
+{
+    public partial class FormUtama : Form
+    {
+        // 1. Setup String Koneksi
+        SqlConnection conn = new SqlConnection("Server=LAPTOP-M60LBIQK\\ZIDANEAS;Database=DB_TokoSembako;Integrated Security=True;");
+
+        public FormUtama()
+        {
+            InitializeComponent();
+        }
+
+        // 2. Fungsi yang otomatis jalan pas aplikasi dibuka
+        private void FormUtama_Load(object sender, EventArgs e)
+        {
+            CekKoneksiDatabase();
+        }
+
+        // 3. Logika untuk ngecek koneksi
+        private void CekKoneksiDatabase()
+        {
+            try
+            {
+                conn.Open(); // Coba buka gerbang ke database
+
+                // Syarat UCP: Muncul notifikasi sukses
+                MessageBox.Show("Koneksi ke Database DB_TokoSembako Berhasil!", "Status Koneksi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                conn.Close(); // Langsung tutup lagi biar aman
+            }
+            catch (Exception ex)
+            {
+                // Kalau error, munculin pesan errornya
+                MessageBox.Show("Gagal terhubung ke database!\nError: " + ex.Message, "Koneksi Gagal", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
