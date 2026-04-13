1a744b32bc271d0259bf1edc8727c87e261fc0f8
ZidaneAlhakim <zidanealhakim179@gmail.com>
2026-04-13 22:43:36 +0700

    koneksi ke database berhasil

--- Jijehh_TokoSembako/FormUtama.Designer.cs
+++ Jijehh_TokoSembako/FormUtama.Designer.cs
@@ -46,12 +46,11 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(108, 52);
+            this.label1.Location = new System.Drawing.Point(191, 74);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.Size = new System.Drawing.Size(67, 16);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
-            
+            this.label1.Text = "ID Barang";
             // 
             // FormUtama
             // 
