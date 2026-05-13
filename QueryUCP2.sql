5e21588188651977f7f0bf601e5cd41ddf44a63e
ZidaneAlhakim <zidanealhakim179@gmail.com>
2026-05-13 09:39:35 +0700

    menambahkan SP untuk insert

--- QueryUCP2.sql
+++ QueryUCP2.sql
@@ -1,3 +1,17 @@
 CREATE VIEW vw_TampilBarang AS
 SELECT * FROM Barang;
 GO
+
+CREATE PROCEDURE sp_InsertBarang
+    @ID_Barang VARCHAR(5),
+    @Nama_Barang VARCHAR(50),
+    @Kategori VARCHAR(25),
+    @Stok SMALLINT,
+    @Harga_Jual DECIMAL(9,0)
+AS
+BEGIN
+    SET NOCOUNT ON;
+    INSERT INTO Barang (ID_Barang, Nama_Barang, Kategori, Stok, Harga_Jual)
+    VALUES (@ID_Barang, @Nama_Barang, @Kategori, @Stok, @Harga_Jual)
+END
+GO
