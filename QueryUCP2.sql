b6aa420e13193b4e2806e2f8591397315a2b0978
ZidaneAlhakim <zidanealhakim179@gmail.com>
2026-05-13 09:40:09 +0700

    menambahkan SP Update barang

--- QueryUCP2.sql
+++ QueryUCP2.sql
@@ -15,3 +15,19 @@ BEGIN
     VALUES (@ID_Barang, @Nama_Barang, @Kategori, @Stok, @Harga_Jual)
 END
 GO
+
+
+CREATE PROCEDURE sp_UpdateBarang
+    @ID_Barang VARCHAR(5),
+    @Nama_Barang VARCHAR(50),
+    @Kategori VARCHAR(25),
+    @Stok SMALLINT,
+    @Harga_Jual DECIMAL(9,0)
+AS
+BEGIN
+    SET NOCOUNT ON;
+    UPDATE Barang 
+    SET Nama_Barang = @Nama_Barang, Kategori = @Kategori, Stok = @Stok, Harga_Jual = @Harga_Jual
+    WHERE ID_Barang = @ID_Barang
+END
+GO
